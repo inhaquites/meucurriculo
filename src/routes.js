378fc8d7cabaa9c44c8b80a21558f7e5be6042d1
inhaquites <r.inhaquites@gmail.com>
2020-04-03 21:28:12 -0300

    nova rota de teste

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const routes = express.Router();
 
+routes.get('/teste', (req, res) => {
+  res.send('teste');
+});
+
 routes.get('/', (req, res) => {
   return res.json({
     "name": "Rodrigo Inhaquites.",
--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Umbler escutanco na porta ', port);
+  console.log('Umbler escutando na porta ', port);
 });
 
